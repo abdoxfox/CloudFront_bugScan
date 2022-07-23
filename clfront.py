3356f2bd3b6dba39daf13bc54ad46e2301ec8f72
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2022-07-24 00:18:31 +0100

    Add files via upload

--- clfront.py
+++ clfront.py
@@ -22,10 +22,11 @@ ipranges={"CLOUDFRONT_GLOBAL_IP_LIST": ["120.52.22.96/27", "205.251.249.0/24", "
  
 frstarray=ipranges["CLOUDFRONT_GLOBAL_IP_LIST"]
 secondarray=ipranges["CLOUDFRONT_REGIONAL_EDGE_IP_LIST"]
+	
 
 def scanner(host):
 	sock=socket.socket()
-	sock.settimeout(2)
+	sock.settimeout(5)
 	try:
 		sock.connect((str(host),80))
 		payload='GET / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(host)
@@ -39,7 +40,9 @@ def scanner(host):
 				try:
 					if data[1] ==' CloudFront':
 						print('{}server : {}\nFound working {}..'.format(G,host,GR))
-						with open('wrCloudfrontIp.txt','a') as fl:fl.write(str(host)+'\n');fl.close()
+						with open('wrCloudfrontIp.txt','a') as fl:
+							fl.write(str(host)+'\n')
+							fl.close()
 				except Exception as e:
 					print(e)
 	except Exception as e:print(e)
@@ -48,14 +51,14 @@ def Main():
 	for k,v in ipranges.items():
 		print('{',k,' : ',v,'}',end='\n')
 	dicts=[frstarray,secondarray]
-	choose = int(input('enter dict number of cloudront ipranges (1/2) : '.title()))
+	choose = int(input('enter dict number of cloudront ipranges (1/2) : '.title()))-1
 	cidrs_list = dicts[choose]
-	for ip in range(len(dicts[int(choose)])):
-		iprange=[]
+	iprange=[]
+	for ips in range(len(dicts[int(choose)])):	
 		for cidr in cidrs_list:
 			for ip in ipcalc.Network(cidr):
 				iprange.append(ip)
-			for index in range(len(iprange)):			
+	for index in range(len(iprange)):			
 					try:
 						print("{}[INFO] Probing... ({}/{}) [{}]{}".format(
 						R,index+1,len(iprange),iprange[index],GR))
@@ -64,5 +67,4 @@ def Main():
 					except KeyboardInterrupt:
 						print('{}Scan aborted by user!{}'.format(R,GR))
 						break
-				
-Main()	
+Main()				
